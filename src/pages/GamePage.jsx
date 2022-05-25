58b179498ca8ac4d52bbd5d2c28738210ed00e92
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-05-25 10:32:21 +0200

    Send empty gameboard to opponent so that it can be rendered the same way as myBoard.

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,26 +1,24 @@
-import MyGameBoard from "../components/MyGameBoard";
-import OpponentGameBoard from "../components/OpponentGameBoard";
+import MyGameBoard from "../components/MyGameBoard"
+import OpponentGameBoard from "../components/OpponentGameBoard"
 
-import useGetShips from "../hooks/useGetShips";
-import { useEffect } from "react";
+import useGetShips from "../hooks/useGetShips"
+import { useEffect } from "react"
 
 export default function GamePage() {
-  const myBoard = useGetShips();
+	const myBoard = useGetShips()
+	const opponentBoard = useGetShips()
 
-  useEffect(() => {
-    myBoard.placeAllShips();
-  }, []);
+	useEffect(() => {
+		myBoard.placeAllShips()
+	}, [])
 
-  console.table(myBoard);
-  console.table(myBoard.battleboard);
-
-  return (
-    <>
-      <h1 className="title">Battleships 🚢💣</h1>
-      <div className="gamepage-container">
-        <MyGameBoard myBoard={myBoard.battleboard} />
-        <OpponentGameBoard />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<h1 className="title">Battleships 🚢💣</h1>
+			<div className="gamepage-container">
+				<MyGameBoard myBoard={myBoard.battleboard} />
+				<OpponentGameBoard opponentBoard={opponentBoard.gameboard()} />
+			</div>
+		</>
+	)
 }
