3b7dc9a9b6d8fdbe305211e82dfde8fc41066f9f
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-05-25 10:20:03 +0200

    Now renders all boxes and shows the index of the box in the grid.

--- src/components/MyGameBoard.jsx
+++ src/components/MyGameBoard.jsx
@@ -1,56 +1,21 @@
-import React from "react";
-
 export default function MyGameBoard({ myBoard }) {
-  /*   const battleships = [
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-  ];
-
-  return (
-    <>
-      <div className="gameboard my-gameboard">
-        {React.Children.toArray(
-          battleships.map((ship) => (
-            <div className="row">
-              {React.Children.toArray(
-                ship.map((item) => <div className="box my-box">{item}</div>)
-              )}
-            </div>
-          ))
-        )}
-      </div>
-    </>
-  ); */
-
-  console.log(myBoard);
-
-  console.log("Här är den spreadade", [...myBoard[0]]);
-
-  return (
-    <div className="gameboard my-gameboard">
-      {myBoard.forEach(
-        (row) => (
-          <div className="row">
-            {row.map((element) => {
-              <div className="box my-box">{element}</div>;
-            })}
-          </div>
-        )
-
-        // <div className="row">
-        //   {row.map((box) => (
-        //     <div className="box my-box">{box}</div>
-        //   ))}
-        // </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="battleboard my-battleboard">
+			{myBoard.map((row, y) => (
+				<div className="row" key={y}>
+					{row.map((box, x) => {
+						// console.log(`This is a box at ${x}:${y}`, box)
+						return (
+							<div
+								className={`box my-box ${box ? "my-ship" : ""}`}
+								key={x}
+							>
+								{x}:{y}
+							</div>
+						)
+					})}
+				</div>
+			))}
+		</div>
+	)
 }
