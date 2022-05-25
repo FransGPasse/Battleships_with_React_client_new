70bda6888898391881c45366cb37ce0317123c92
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-05-25 15:36:44 +0200

    added id and can now click on box and get id for box

--- src/components/OpponentGameBoard.jsx
+++ src/components/OpponentGameBoard.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 
 export default function OpponentGameBoard({ opponentBoard }) {
+	const clickedBox = (e) => {
+		console.log('Clicked on',e.target.id)
+	}
 	return (
 		<>
 			<div className="gameboard opponent-board">
@@ -9,7 +12,7 @@ export default function OpponentGameBoard({ opponentBoard }) {
 						{row.map((box, x) => {
 							// console.log(`This is a box at ${x}:${y}`, box)
 							return (
-								<div className="box opponent-box" key={x}>
+								<div className="box opponent-box" id={`${x}:${y}`} key={x} onClick={clickedBox}>
 									{x}:{y}
 								</div>
 							)
