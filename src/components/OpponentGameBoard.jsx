d34e4a8bac5843e46e9381b7527c6d937f1a75f7
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-05-27 18:08:16 +0200

    sends which box was clicked to server

--- src/components/OpponentGameBoard.jsx
+++ src/components/OpponentGameBoard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useRoomContext } from "../contexts/RoomContextProvider";
 
 export default function OpponentGameBoard({ opponentBoard }) {
+  const { socket } = useRoomContext();
   const clickedBox = (e) => {
     console.log("Clicked on", e.target.id);
+    socket.emit("clicked_box", e.target.id);
   };
   return (
     <>
