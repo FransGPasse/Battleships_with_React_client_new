3f0dff85d765fc5fad1e058419de4ac28a05465f
Frans Gustavson Påsse <frans.gop@hotmail.com>
2022-05-31 13:40:47 +0200

    Changed App to properly display a disconnect page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import "./assets/App.scss";
 
+import { useState, useEffect } from "react";
+
 import JoinPage from "./pages/JoinPage";
 import GamePage from "./pages/GamePage";
 
+import { useRoomContext } from "./contexts/RoomContextProvider";
+
 function App() {
+  const { socket } = useRoomContext();
+  const [disconnected, setDisconnected] = useState();
+
+  useEffect(() => {
+    socket.on("user_disconnected", () => {
+      setDisconnected(true);
+    });
+  }, [socket]);
+
+  if (disconnected)
+    return (
+      <div className="disconnect-page">
+        <h1 className="disconnect-msg">The other user disconnected...</h1>
+      </div>
+    );
+
   return (
     <div className="App">
       <Routes>
