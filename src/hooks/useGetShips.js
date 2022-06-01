0428f4c9493d0f9bc3ede919a7448ad2dff6eab7
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-06-01 19:36:58 +0200

    Trying to get the logic of rendering ships on button click to work. Made progress here.

--- src/hooks/useGetShips.js
+++ src/hooks/useGetShips.js
@@ -41,52 +41,46 @@ const ships = [
 const useGetShips = () => {
   const [battleboard, setBattleboard] = useState(gameboard);
 
-  const generateEmptyGameboard = () => {
-    const emptyBattleboard = gameboard();
-    setBattleboard(emptyBattleboard);
-  };
-
   // generate starting position
   const generateStartingIndex = () => {
     return Math.floor(Math.random() * 10);
   };
 
   const placeAllShips = () => {
-    generateEmptyGameboard();
-    ships.forEach((ship) => placeShip(ship));
-    // console.table(battleboard)
+    let emptyBattleboard = gameboard();
+    ships.forEach((ship) => placeShip(emptyBattleboard, ship));
+    setBattleboard(emptyBattleboard);
   };
 
-  const placeShip = (ship) => {
+  const placeShip = (emptyBattleboard, ship) => {
     const startingX = generateStartingIndex();
     const startingY = generateStartingIndex();
     let occupied = false;
-    // const currentShipSpaceY = startingY + ship.length
 
     // check if no ship in startingposition and inside board
     if (
-      battleboard[startingY][startingX] === null &&
+      emptyBattleboard[startingY][startingX] === null &&
       10 > startingX + ship.length
     ) {
       // check if any of the ships parts is interfearing with another ship
       for (let i = startingX; i <= startingX + ship.length; i++) {
         // if you find ship anywhere, change occupied
-        if (battleboard[startingY][i] !== null) {
+        if (emptyBattleboard[startingY][i] !== null) {
           occupied = true;
         }
       }
 
       // if no ship-part found, place ship there
       if (!occupied) {
-        const newBattleboard = [...battleboard];
+        const newBattleboard = [...emptyBattleboard];
         newBattleboard[startingY].fill(
           ship,
           startingX,
           startingX + ship.length
         );
 
-        setBattleboard(newBattleboard);
-        return;
+        emptyBattleboard = [...newBattleboard]
+        return emptyBattleboard
       }
     }
     // if any of the criterias fail, call function again to get new coordinates
@@ -97,7 +91,6 @@ const useGetShips = () => {
     battleboard,
     gameboard,
     placeAllShips,
-    generateEmptyGameboard,
   };
 };
 
