df1bea54103821783b4fe8e7d5fb3669e138d443
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-06-06 15:29:00 +0200

    Added state to see if I can shoot or not, and listen to socket to change this state. Onclick now listens to this state.

--- src/components/OpponentGameBoard.jsx
+++ src/components/OpponentGameBoard.jsx
@@ -1,17 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRoomContext } from "../contexts/RoomContextProvider";
 
 export default function OpponentGameBoard({ opponentBoard }) {
   const { socket } = useRoomContext();
+  
+  // state to see if I can shoot or not
+  const [canShoot, setCanShoot] = useState(false)
 
   //Skickar med ID:t för eventet på klick, samt socketens ID
   const clickedBox = (e) => {
     socket.emit("clicked_box", e.target.id, socket.id);
   };
 
+  // Lyssnar på servern om jag får börja skjuta
+  useEffect(() => {
+    socket.on(
+      "you_start",
+      () => setCanShoot(true),
+      console.log("Can I shoot?", canShoot)
+    );
+  }, [socket, canShoot]);
+
+  // Lyssnar på servern om min motståndare får börja skjuta
+  useEffect(() => {
+    socket.on(
+      "not_your_start",
+      () => setCanShoot(false),
+      console.log("Am I waiting for my turn to shoot?", canShoot)
+    );
+  }, [socket, canShoot]);
+
+  // När det kommer en träff/miss, låt mig skjuta
+  useEffect(() => {
+    socket.on("your_turn_to_shoot", () => {
+     setCanShoot(true)
+    });
+  }, [socket]);
+
   //Lyssnar efter svar från servern på om det var en träff eller miss och lägger till korresponderande klass
   useEffect(() => {
     socket.on("server_ship_response", (clickedBoxID, hitShip) => {
+      setCanShoot(false)
       //Skapar variabel för den klickade lådan och hittar den med en querySelector
       let clickedBox = document.querySelector(`#${clickedBoxID}`);
 
@@ -35,7 +64,7 @@ export default function OpponentGameBoard({ opponentBoard }) {
                   //Lägger till "opp-" innan box så att alla lådor har unika ID:n
                   id={`opp-box-${y}-${x}`}
                   key={x}
-                  onClick={clickedBox}
+                  onClick={canShoot ? clickedBox : undefined}
                 >
                   {y}-{x}
                 </div>
