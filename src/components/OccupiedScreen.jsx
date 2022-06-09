6f35d9640efa9cb2444c7d8a9541bf6387f45b12
Frans Gustavson Påsse <frans.gop@hotmail.com>
2022-06-10 00:25:16 +0200

    Removed rests of button from OccupiedScreen

--- src/components/OccupiedScreen.jsx
+++ src/components/OccupiedScreen.jsx
@@ -1,19 +1,14 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
 
 const OccupiedScreen = () => {
-    const navigate = useNavigate()
+  return (
+    <div className="occupied-page">
+      <h1 className="occupied-msg">
+        There are already two players playing. You have to wait for your
+        turn....
+      </h1>
+    </div>
+  );
+};
 
-    const newGame = () => {
-        navigate("/");
-        console.log("start a new game??")
-    }
-
-    return (
-        <div className="occupied-page">
-            <h1 className="occupied-msg">There are already two players playing. You have to wait for your turn....</h1>
-        </div>
-    )
-}
-
-export default OccupiedScreen
+export default OccupiedScreen;
